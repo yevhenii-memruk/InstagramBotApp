ae831a98a6bf09c0079b9bd9552196fdcee29f86
yevhenii-memruk <yevhenii.memruk@gmail.com>
2021-04-27 12:35:13 +0200

    InstaPop | xpath_existing, open_profile, like_exactly_post, like_by_hash

--- main/instapop.py
+++ main/instapop.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from user_data import username, password
 import time
 import random
@@ -26,15 +27,28 @@ class InstaBot:
         self.password = password
         self.browser = webdriver.Chrome("chromedriver.exe")
 
+    # closing tab and quiting the browser
     def exit(self):
         self.browser.close()
         self.browser.quit()
 
+    # checking by xpath if element exist
+    def xpath_existing(self, url):
+        browser = self.browser
+        try:
+            browser.find_element_by_xpath(url)
+            exist = True
+        except NoSuchElementException:
+            exist = False
+        return exist
+
+    # signing into profile
     def sign_in(self):
         browser = self.browser
         try:
             browser.get("https://www.instagram.com/")
             browser.implicitly_wait(10)
+            # closing pop-up window
             browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
             time.sleep(1)
 
@@ -51,13 +65,73 @@ class InstaBot:
                 password_tag.send_keys(step_typing)
             time.sleep(1)
             password_tag.send_keys(Keys.ENTER)
+            time.sleep(2)
 
         except Exception as ex:
             print(ex)
             self.exit()
 
-    
+    def open_profile(self, link):
+        browser = self.browser
+        browser.get(link)
+        time.sleep(random.randrange(3))
+
+    # liking post by certain link
+    def like_exactly_post(self, link_post):
+        browser = self.browser
+        time.sleep(3)
+        browser.get(link_post)
+        time.sleep(3)
+
+        wrong_userpage = "/html/body/div[1]/section/main/div/h2"
+        if self.xpath_existing(wrong_userpage):
+            print("The post does not exist!")
+        else:
+            print("Post detected! Put our like!")
+
+        like = browser.find_elements_by_css_selector("svg[fill = '#ed4956']")
+        if like:
+            print("Like already put")
+            self.exit()
+        else:
+            browser.find_element_by_xpath(
+                "/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button").click()
+            time.sleep(2, 4)
+            self.exit()
+
+    def like_by_hash(self, hashtag):
+        global posts
+        browser = self.browser
+
+        browser.get(f"https://www.instagram.com/explore/tags/{hashtag}/")
+        time.sleep(5)
+
+        for x in range(1, 5):
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(random.randrange(3, 5))
+
+            refs = browser.find_elements_by_tag_name("a")
+
+            posts = [i.get_attribute("href") for i in refs if "/p/" in i.get_attribute("href")]
+            # for i in refs:
+            #     href = i.get_attribute("href")
+            #     if "/p/" in href:
+            #        posts.append(href)
+
+        for y in posts[0:1]:
+            browser.get(y)
+
+            like = browser.find_elements_by_css_selector("svg[fill = '#ed4956']")
+            if like:
+                print("Like already put")
+                self.exit()
+            else:
+                browser.find_element_by_xpath(
+                    "//*[@id=\"react-root\"]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button").click()
+                time.sleep(5)
+                self.exit()
 
 test = InstaBot(username, password)
 
 test.sign_in()
+test.like_by_hash("dog")
